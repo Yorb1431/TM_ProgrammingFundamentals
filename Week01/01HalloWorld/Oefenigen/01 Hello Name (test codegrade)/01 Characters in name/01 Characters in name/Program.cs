0cf1ae476b9b92a7cd7c0f528c35d4a1142d98e0
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-09-19 15:46:22 +0200

    Oefing done lenght

---
+++ Week01/01HalloWorld/Oefenigen/01 Hello Name (test codegrade)/01 Characters in name/01 Characters in name/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Basis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName = Console.ReadLine();
+            string LastName = Console.ReadLine();
+            int charactersFirstName = FirstName.Length;
+            int charactersLastName = LastName.Length;
+
+            Console.WriteLine($"{FirstName} {LastName}, your first name has {charactersFirstName} characters and your last name has {charactersLastName}.");
+        }
+    }
+}
