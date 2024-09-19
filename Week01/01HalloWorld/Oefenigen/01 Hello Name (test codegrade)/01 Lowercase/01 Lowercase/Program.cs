db7a6b4ebaf49e3a934342541fdd1f8dd2363069
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-09-19 14:31:44 +0200

    Create fill 01 Lowercase

---
+++ Week01/01HalloWorld/Oefenigen/01 Hello Name (test codegrade)/01 Lowercase/01 Lowercase/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Basis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your first name? ");
+
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine($"HELLO {FirstName.ToUpper()}");
+
+        }
+    }
+}
