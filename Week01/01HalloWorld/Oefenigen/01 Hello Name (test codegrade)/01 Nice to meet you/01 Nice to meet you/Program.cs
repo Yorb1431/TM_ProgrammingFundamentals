8d055a63d6efb3fd6ac5433be70e14fd5351ff39
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-09-19 15:54:33 +0200

    /n trim (nxtline) 01 nice to meet you

---
+++ Week01/01HalloWorld/Oefenigen/01 Hello Name (test codegrade)/01 Nice to meet you/01 Nice to meet you/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Basis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName = Console.ReadLine().Trim();
+
+            Console.WriteLine($"Nice to meet you\n{FirstName}");
+        }
+    }
+}
