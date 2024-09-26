541846c41411f77846dd2ba553767354abc16b03
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-09-26 09:48:03 +0200

    System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");  bij werkenb met comma getallen add line on top

--- Week02/02Variable/ProjectenKlasikaal/Oefening01/02Variable/Program.cs
+++ Week02/02Variable/ProjectenKlasikaal/Oefening01/02Variable/Program.cs
@@ -6,6 +6,7 @@ namespace _02Variable
     {
         static void Main(string[] args)
         {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
             // Werken  rond variable
             // Declareren ervan
 
