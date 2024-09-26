85d4cd32d97fcc26021c1657dccbcb28c20962ad
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-09-26 09:45:07 +0200

    Start kommagetallen  (double)

--- Week02/02Variable/ProjectenKlasikaal/Oefening01/02Variable/Program.cs
+++ Week02/02Variable/ProjectenKlasikaal/Oefening01/02Variable/Program.cs
@@ -25,7 +25,12 @@ namespace _02Variable
             byte nummerByte = 12;
             // nummerByte = 256; ==> Min waarde 0 max Waarden 255
 
-            short
+            short nummerShort = 12000;
+
+            long l = 2349637382892929292;
+
+            // kommagetallen
+            double nummerDouble = 45.896789 //Niet meer als 15 getallen na komma. 
 
 
 
