1e76ee32b00db3555e44118e884d51fd6f55fe20
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-09-26 12:48:32 +0200

    finished product

--- Week02/02Variable/ProjectenKlasikaal/InLesPrograms/Program02InLes_ValidAge/02ValidAge/Program.cs
+++ Week02/02Variable/ProjectenKlasikaal/InLesPrograms/Program02InLes_ValidAge/02ValidAge/Program.cs
@@ -6,7 +6,10 @@ namespace _02ValidAge
         static void Main(string[] args)
         {
 
-            
+            int leeftijd = Convert.ToInt32(Console.ReadLine());
+            int LegaleBerekening = leeftijd / 2 + 7;
+            Console.WriteLine($"Je leeftijd is {leeftijd} en je mag zeker en vast " +
+                $"iemand daten vanaf {LegaleBerekening}");
 
 
 
