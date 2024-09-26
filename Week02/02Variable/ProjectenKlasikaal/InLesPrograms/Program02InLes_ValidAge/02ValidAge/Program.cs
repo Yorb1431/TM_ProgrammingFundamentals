bc08610204b139bbaf3ff01d1134f69d16dbc512
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-09-26 12:39:35 +0200

    Creating Files 02ValidAge project

---
+++ Week02/02Variable/ProjectenKlasikaal/InLesPrograms/Program02InLes_ValidAge/02ValidAge/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02ValidAge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
