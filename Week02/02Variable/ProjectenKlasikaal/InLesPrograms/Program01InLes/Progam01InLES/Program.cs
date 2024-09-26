5d9cf245152990bc2f034106efa318e14137f9ce
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-09-26 11:46:54 +0200

    Creating new files  Progam01InLES and startup program

---
+++ Week02/02Variable/ProjectenKlasikaal/InLesPrograms/Program01InLes/Progam01InLES/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Progam01InLES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //input en output
+            //omzetten van datatypes
+
+            Console.WriteLine("Geef een naam: ");
+            string naam = Console.ReadLine();
+            Console.WriteLine(naam);
+
+
+            //omzetting
+            Console.Write("Geef een getal: ");
+            string antwoord= Console.ReadLine();
+            int getal = Convert.ToInt32(antwoord);
+            Console.WriteLine(antwoord);
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
