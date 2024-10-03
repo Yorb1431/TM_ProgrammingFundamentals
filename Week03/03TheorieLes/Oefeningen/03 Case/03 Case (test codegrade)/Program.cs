bfa1d12117eb2149edd2ce788c700437d75b4105
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-03 12:24:48 +0200

    creating file and make excercise

---
+++ Week03/03TheorieLes/Oefeningen/03 Case/03 Case (test codegrade)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03_Case__test_codegrade_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = Convert.ToChar(Console.ReadLine());
+            int asci = (int)letter;
+
+            if (asci >= 97 && asci <= 122)
+            {
+                Console.WriteLine("lowercase");
+            }
+            else if (asci >= 65 && asci <= 90)  // Fixed condition here
+            {
+                Console.WriteLine("uppercase");
+            }
+            else
+            {
+                Console.WriteLine("Crazy input");
+            }
+        }
+    }
+}
