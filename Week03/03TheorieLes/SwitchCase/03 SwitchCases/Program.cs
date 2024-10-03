d6c65ca7121dd3606f312bc8993137df2891a915
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-03 11:11:45 +0200

    verjaardag printed out

--- Week03/03TheorieLes/SwitchCase/03 SwitchCases/Program.cs
+++ Week03/03TheorieLes/SwitchCase/03 SwitchCases/Program.cs
@@ -15,6 +15,12 @@ namespace _03_SwitchCases
             //Console.WriteLine(DateTime.Now); --> alle gegevens die we bijhouden
 
             DateTime yesterday = new DateTime(2024, 10, 02);
+            string dag = yesterday.DayOfWeek.ToString();
+            Console.WriteLine(yesterday); // 02/10/2024 00:00:00
+
+            DateTime verjaardag = new DateTime(2005, 05, 11); //jjjj,mm,dd
+            dag = verjaardag.DayOfWeek.ToString();
+            Console.WriteLine(dag);
 
 
 
