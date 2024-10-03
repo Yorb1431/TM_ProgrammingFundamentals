4ed89016c3d3d1b79acd7e46008a1ebe4b169a52
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-03 12:39:39 +0200

    Convert.ToDouble

--- Week03/03TheorieLes/Oefeningen/03 HittenGolf/03_HittenGolf/Program.cs
+++ Week03/03TheorieLes/Oefeningen/03 HittenGolf/03_HittenGolf/Program.cs
@@ -12,6 +12,16 @@ namespace _03_HittenGolf
              * If this is the case, output: BELGIUM IS ON FIRE
              * If not output: It's probably raining again.
              * temp has to be a double! */
+
+
+            double day1 = Convert.ToDouble(Console.ReadLine());
+            double day2 = Convert.ToDouble(Console.ReadLine());
+            double day3 = Convert.ToDouble(Console.ReadLine());
+            double day4 = Convert.ToDouble(Console.ReadLine());
+            double day5 = Convert.ToDouble(Console.ReadLine());
+
+
+
         }
     }
 }
