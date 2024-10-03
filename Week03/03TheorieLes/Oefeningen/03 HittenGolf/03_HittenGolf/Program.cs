ac75ccb788496945e9b987552403422b5af6df28
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-03 12:37:41 +0200

    Creating Files HitteGolf

---
+++ Week03/03TheorieLes/Oefeningen/03 HittenGolf/03_HittenGolf/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace _03_HittenGolf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //HitteGolfCalculater
+            /* Write a programme that asks for the temperature of 5 days in a row.
+             * Check whether or not this temperature is higher than 25 ° C
+             * also check if 3 of the 5 days are higher than 30°C
+             * If this is the case, output: BELGIUM IS ON FIRE
+             * If not output: It's probably raining again.
+             * temp has to be a double! */
+        }
+    }
+}
