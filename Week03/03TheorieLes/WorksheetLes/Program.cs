50605ef977d66462a592db7b006fc95251250931
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-03 10:11:12 +0200

    first if statemnt x else / if else

--- Week03/03TheorieLes/WorksheetLes/Program.cs
+++ Week03/03TheorieLes/WorksheetLes/Program.cs
@@ -1,11 +1,29 @@
 ﻿using System;
+
 namespace WorksheetLes
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Prompt user for input
+            Console.WriteLine("Geef een getal");
+
+            // Try to parse input as an integer
+            if (int.TryParse(Console.ReadLine(), out int getal))
+            {
+                // If statements
+                if (getal < 50){
+                    Console.WriteLine("Getal kleiner dan 50");
+                }
+                else if (getal == 50){
+
+                    Console.WriteLine("Getal is 50");
+                }
+                else{
+                    Console.WriteLine("Getal is groter dan 50");
+                }
+            }
         }
     }
 }
