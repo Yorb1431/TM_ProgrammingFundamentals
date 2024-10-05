b78fc12aa91482a302d40c0db9722e830393dd8b
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-05 22:36:46 +0200

    Finished project

--- Week02/Exercises/02Even/02Even/Program.cs
+++ Week02/Exercises/02Even/02Even/Program.cs
@@ -20,7 +20,11 @@ namespace Basis
                     output:
                         True             
             */
+            // Read in an integer from the user
+            int number = Convert.ToInt32(Console.ReadLine());
 
+            // Check if the number is even using modulo, and print the result directly as a boolean expression
+            Console.WriteLine(number % 2 == 0);  // This will print True if the number is even, False if odd
 
 
 
