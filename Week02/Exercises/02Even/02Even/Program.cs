ce9f54edce84a81c2e40c51e2fa71aa7d0f25066
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-05 22:32:35 +0200

    Creating New Files

---
+++ Week02/Exercises/02Even/02Even/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Basis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Read in an integer. Print True if the number is even, otherwise print False.
+
+                Input/Output
+                    input:
+                        37
+                    output:
+                        False
+
+                   input:
+                        -12
+                    output:
+                        True             
+            */
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
