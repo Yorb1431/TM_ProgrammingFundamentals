025f5f300f11d371e6d2d0a83ff80a7fda44e60a
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-05 22:10:21 +0200

    Opdracht intonotes

--- Week02/Exercises/Ascii_Oefening/02Ascii/Program.cs
+++ Week02/Exercises/Ascii_Oefening/02Ascii/Program.cs
@@ -6,7 +6,38 @@ namespace Basis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             we can easily convert between char and int (as we've seen in class)
+               check out String-library!
+ 
+
+                Input/Output
+                    input:
+                        66
+                    output:
+                        b
+                        B
+
+                    input:
+                        107
+                    output:
+                        k
+                        K
+             */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
