32edc1263d64d86dc22440415692877a94bfd3e1
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-06 12:50:13 +0200

    Finsisching full asigment

--- Week02/Exercises/02_Minutes/02_Minutes/Program.cs
+++ Week02/Exercises/02_Minutes/02_Minutes/Program.cs
@@ -8,22 +8,32 @@ namespace Basis
         {
             /*
                 Read in minutes and convert it to seconds and hours.
-
- 
-
-                Input/Output
+                
+                Input/Output:
                         input:
                             71
                         output:
                             4260 seconds
                             1 hour(s)
-
                         input:
                             508
                         output:
                             30480 seconds
-                            8 hour(s)             
+                            8 hour(s)
              */
+
+            // Read in the number of minutes
+            int minutes = Convert.ToInt32(Console.ReadLine());
+
+            // Convert minutes to seconds
+            int seconds = minutes * 60;
+
+            // Convert minutes to hours
+            int hours = minutes / 60;
+
+            // Output the result
+            Console.WriteLine(seconds + " seconds");
+            Console.WriteLine(hours + " hour(s)");
         }
     }
 }
