28401683d2a660e3b43efd6030271b275e6c2ce0
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-06 13:50:36 +0200

    Finishing assigment Circumference

--- Week02/Exercises/02_Circumference/02_Circumference/Program.cs
+++ Week02/Exercises/02_Circumference/02_Circumference/Program.cs
@@ -7,9 +7,7 @@ namespace Basis
         static void Main(string[] args)
         {
             /*
-                Read in length and width of a rectangle and calculate its circumference using the formula 2*(l + w).
-
- 
+                Read in the length and width of a rectangle and calculate its circumference using the formula 2*(l + w).
 
                 Input/Output
                     input:
@@ -18,13 +16,22 @@ namespace Basis
                     output:
                         88
                 
-                
                     input:
                         11
                         3
                     output:
                         28
              */
+
+            // Read in the length and width from the user
+            int lengte = Convert.ToInt32(Console.ReadLine());
+            int breedte = Convert.ToInt32(Console.ReadLine());
+
+            // Calculate the circumference
+            int circumference = 2 * (lengte + breedte);
+
+            // Print the result
+            Console.WriteLine(circumference);
         }
     }
 }
