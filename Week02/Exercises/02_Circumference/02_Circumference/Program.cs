54bf9088a63ea8cc58811afe7fc8e9103eb4e84f
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-06 13:47:04 +0200

    Creating ne wfiles

---
+++ Week02/Exercises/02_Circumference/02_Circumference/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Basis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Read in length and width of a rectangle and calculate its circumference using the formula 2*(l + w).
+
+ 
+
+                Input/Output
+                    input:
+                        9
+                        35
+                    output:
+                        88
+                
+                
+                    input:
+                        11
+                        3
+                    output:
+                        28
+             */
+        }
+    }
+}
