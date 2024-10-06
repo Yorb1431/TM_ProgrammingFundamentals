c847168db814a8c79d267f2bd15fa89540adafa0
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-06 12:35:11 +0200

    Creating files 02_Decimal_part

---
+++ Week02/Exercises/02_Decimal_part/02_Decimal_part/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Basis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Read in a number and print out the decimal part of the number (round to 6 digits).
+
+             Problems with . as the decimal separator? Add this line of code in your Main-method before you start reading input from the console:
+             System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+             
+            Input/Output
+                input:
+                    13.62001
+                output:
+                    Decimal part = 0.620010
+
+                input:
+                    26.389
+                output:
+                    Decimal part = 0.389000             
+             */
+
+
+
+
+        }
+    }
+}
