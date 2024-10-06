7857df0eead9b408e887e26ec40cead88788c043
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-06 13:41:10 +0200

    Creating files and finishing devicint

--- Week02/Exercises/02_Divide/02_Divide/Program.cs
+++ Week02/Exercises/02_Divide/02_Divide/Program.cs
@@ -7,23 +7,30 @@ namespace Basis
         static void Main(string[] args)
         {
             /*
-             Read in two integers, a and b, print true if a can be divided evenly by b. print false otherwise.
-
- 
-
+             Read in two integers, a and b, print true if a can be divided evenly by b. Print false otherwise.
+             
                 Input/Output
-                        input:
-                            12
-                            3
-                        output:
-                            True
-
-                        input:
-                            12
-                            8
-                        output:
-                            False             
+                    input:
+                        12
+                        3
+                    output:
+                        True
+                    input:
+                        12
+                        8
+                    output:
+                        False
              */
+
+            // Read in two integers from the user
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            // Check if a is divisible by b
+            bool antwoord = (a % b == 0);
+
+            // Print the result (true or false)
+            Console.WriteLine(antwoord);
         }
     }
 }
---
+++ Week02/Exercises/02_Reverse/02_Reverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Basis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Read in three characters and display them in reverse order.
+
+                Input/Output
+                        input:
+                            t
+                            e
+                            b
+                        output:
+                            bet
+
+
+                        input:
+                            n
+                            u
+                            r
+                        output:
+                            run
+             */
+        }
+    }
+}
