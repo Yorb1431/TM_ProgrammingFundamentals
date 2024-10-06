f7f5b10f9b1d262c15ea2399d21250ddcc33f9f7
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-06 13:56:19 +0200

    02_BMI finished

--- Week02/Exercises/02_BMI/02_BMI/Program.cs
+++ Week02/Exercises/02_BMI/02_BMI/Program.cs
@@ -7,28 +7,29 @@ namespace Basis
         static void Main(string[] args)
         {
             /*
-                Read in the person's weight in kilograms (kg) and their height in metres (m). Calculate the person's BMI and round it to 2 digits.
+                Read in the person's weight in kilograms (kg) and their height in metres (m). 
+                Calculate the person's BMI and round it to 2 digits.
 
                 Use the following formula: BMI = kg / m²
 
                 Problems with . as the decimal separator? Add this line of code in your Main-method before you start reading input from the console:
                 System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            */
 
- 
+            // Add this line to handle . as the decimal separator
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
 
-                Input/Output
-                        input:
-                            76
-                            1.89
-                        output:
-                            21.28
+            // Read in the person's weight in kg
+            double gewicht = Convert.ToDouble(Console.ReadLine());
 
-                        input:
-                            59
-                            1.75
-                        output:
-                            19.27             
-             */
+            // Read in the person's height in meters
+            double lengte = Convert.ToDouble(Console.ReadLine());
+
+            // Calculate the BMI
+            double BMI = gewicht / (lengte * lengte);
+
+            // Print the rounded BMI value (rounded to 2 decimal places)
+            Console.WriteLine(Math.Round(BMI, 2));
         }
     }
 }
