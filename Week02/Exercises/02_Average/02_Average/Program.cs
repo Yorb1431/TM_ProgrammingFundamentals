6a7c2fe66c8e8f341e9d640e6c44bcb82af04d06
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-06 13:59:46 +0200

    Creating files Avarege

---
+++ Week02/Exercises/02_Average/02_Average/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Basis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Read in five numbers and calculate the average. Print average as an integer.
+
+                Problems with . as the decimal separator? Add this line of code in your Main-method before you start reading input from the console:
+
+                System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+
+                Input/Output
+                        input:
+                            17.15
+                            -3
+                            9
+                            6.4
+                            2
+                        output:
+                            6
+
+                        input:
+                            22.71
+                            -15
+                            7
+                            3.945
+                            2
+                        output:
+                            4
+               */
+        }
+    }
+}
