6e8cb8663d936f88cd58008b91dfb82192ee5975
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-06 13:53:37 +0200

    Creating files 02_BMI

---
+++ Week02/Exercises/02_BMI/02_BMI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Basis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Read in the person's weight in kilograms (kg) and their height in metres (m). Calculate the person's BMI and round it to 2 digits.
+
+                Use the following formula: BMI = kg / m²
+
+                Problems with . as the decimal separator? Add this line of code in your Main-method before you start reading input from the console:
+                System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+
+ 
+
+                Input/Output
+                        input:
+                            76
+                            1.89
+                        output:
+                            21.28
+
+                        input:
+                            59
+                            1.75
+                        output:
+                            19.27             
+             */
+        }
+    }
+}
