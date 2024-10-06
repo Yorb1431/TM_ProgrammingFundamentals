5c89022ad1a3f9460517fc8c1140e9dc4312a82d
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-06 12:47:15 +0200

    Creating file 02 Minutes and using system

---
+++ Week02/Exercises/02_Minutes/02_Minutes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Basis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Read in minutes and convert it to seconds and hours.
+
+ 
+
+                Input/Output
+                        input:
+                            71
+                        output:
+                            4260 seconds
+                            1 hour(s)
+
+                        input:
+                            508
+                        output:
+                            30480 seconds
+                            8 hour(s)             
+             */
+        }
+    }
+}
