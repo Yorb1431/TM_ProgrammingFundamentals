fdef9ea680a468c418accefae193e5c1f5f6504c
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-06 13:36:18 +0200

    Creating file and addingusingsystem

---
+++ Week02/Exercises/02_Divide/02_Divide/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Basis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Read in two integers, a and b, print true if a can be divided evenly by b. print false otherwise.
+
+ 
+
+                Input/Output
+                        input:
+                            12
+                            3
+                        output:
+                            True
+
+                        input:
+                            12
+                            8
+                        output:
+                            False             
+             */
+        }
+    }
+}
