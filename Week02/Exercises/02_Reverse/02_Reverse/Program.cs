60366c45487d8f846556d448650486de9099c44c
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-06 13:43:52 +0200

    Assigment done  Reverse02

--- Week02/Exercises/02_Reverse/02_Reverse/Program.cs
+++ Week02/Exercises/02_Reverse/02_Reverse/Program.cs
@@ -10,21 +10,27 @@ namespace Basis
                 Read in three characters and display them in reverse order.
 
                 Input/Output
-                        input:
-                            t
-                            e
-                            b
-                        output:
-                            bet
-
-
-                        input:
-                            n
-                            u
-                            r
-                        output:
-                            run
+                    input:
+                        t
+                        e
+                        b
+                    output:
+                        bet
+                    input:
+                        n
+                        u
+                        r
+                    output:
+                        run
              */
+
+            // Read in three characters from the user
+            char first = Convert.ToChar(Console.ReadLine());
+            char second = Convert.ToChar(Console.ReadLine());
+            char third = Convert.ToChar(Console.ReadLine());
+
+            // Print the characters in reverse order
+            Console.WriteLine($"{third}{second}{first}");
         }
     }
 }
