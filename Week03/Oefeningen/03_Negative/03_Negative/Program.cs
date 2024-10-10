11137ad2f1482a0840ca9ae20bf58812138c3ae5
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-10 15:02:57 +0200

    Assigment in comments

--- Week03/Oefeningen/03_Negative/03_Negative/Program.cs
+++ Week03/Oefeningen/03_Negative/03_Negative/Program.cs
@@ -5,7 +5,24 @@ namespace _03_Negative
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+                Read in a number and return the negative number when a number is already negative, nothing changes, when the number is 0, print 'error converting'.
+
+ 
+
+                Input/Output
+                        input:
+                            183
+                        output:
+                            -183
+
+                        input:
+                            -26
+                        output:
+                             -26
+             */
+
+
         }
     }
 }
