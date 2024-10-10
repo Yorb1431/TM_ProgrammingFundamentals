6aa7f0138342ca2e5e1eb9dd8e40264f679552a0
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-10 12:09:23 +0200

    Pyramide 3 error/errorfix

--- Week04/OefeningenInLes/04Pyramide/04Pyramides/Program.cs
+++ Week04/OefeningenInLes/04Pyramide/04Pyramides/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-namespace _04Pyramides
+﻿namespace _04Pyramides
 {
     internal class Program
     {
@@ -9,6 +8,85 @@ namespace _04Pyramides
             // * *
             // * * *
             // * * * *
+            Console.WriteLine("\nPryramide 01");
+            int x = 4;
+            for (int i = 1; i <= x; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine("");
+            }
+
+
+            // * * * *
+            // * * *
+            // * *
+            // *
+            Console.WriteLine("\nPryramide 02");
+            for (int i = 0; i <= x; i++)
+            {
+                for (int j = x; j > i; j--)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("\nPryramide 03");
+            // + + + *
+            // + + * *
+            // + * * *
+            // * * * *
+            for (int i = 1; i <= x; i++)
+            {
+                for (int j = x; j > i; j--)
+                {
+                    Console.Write("  ");
+                }
+                for (int j = 0; j < 1; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("test");
+
+            for (int i = 1; i <= x; i++)
+            {
+                for (int j = x; j > i; j--)
+                {
+                    Console.Write("  ");
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nPryramide 04");
+
+            // * * * *
+            // + * * *
+            // + + * *
+            // + + + *
+
+            Console.WriteLine("Pyramid 4");
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("  ");
+                }
+                for (int j = x; j > i; j--)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
 
         }
     }
