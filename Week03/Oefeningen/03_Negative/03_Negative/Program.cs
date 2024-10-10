b508ef4fcd6d8e9407cd2056d74fb00baffdeb3d
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-10 15:01:31 +0200

    Creating Files 03_Negative

---
+++ Week03/Oefeningen/03_Negative/03_Negative/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace _03_Negative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
