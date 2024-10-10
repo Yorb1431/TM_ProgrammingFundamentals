6772531280c432d8331f9ab8dcf0377e5ebaed0b
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-10 12:13:15 +0200

    Creating files 04While

---
+++ Week04/OefeningenInLes/04While/04While/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _04While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //WHILE
+            Console.Write("START! press y to continue: ");
+            string answer = Console.ReadLine();
+
+            while (answer == "y")
+            {
+                Console.Write("press y to continue again: ");
+                answer = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
