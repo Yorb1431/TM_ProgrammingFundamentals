16570a284d4b2bcd85b1d878c6ecf64616d4d502
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-10 09:24:11 +0200

    Betere versie

--- Week04/04WeekInLesTheory/04WeekInLesTheory/Program.cs
+++ Week04/04WeekInLesTheory/04WeekInLesTheory/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace _04WeekInLesTheory
 {
     internal class Program
@@ -10,6 +11,15 @@ namespace _04WeekInLesTheory
             int getal = Convert.ToInt32(Console.ReadLine());
 
             //Idiote versie
+            Console.WriteLine($"{getal} * 1 = {getal * 1}");
+            Console.WriteLine($"{getal} * 2 = {getal * 2}");
+            Console.WriteLine($"{getal} * 3 = {getal * 3}");
+            Console.WriteLine($"{getal} * 4 = {getal * 4}");
+
+
+
+            // Betere versie
+
         }
     }
 }
