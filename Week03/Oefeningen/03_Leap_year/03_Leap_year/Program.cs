fe0b0c26783c0d0255c652627a282bdf4b2023a2
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-10 15:22:29 +0200

    Creating files 03_Leap_year

---
+++ Week03/Oefeningen/03_Leap_year/03_Leap_year/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace _03_Leap_year
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Read in a year (integer) and print whether or not this year is/was a leap year.
+
+                How to calculate a leap year:
+
+                Has to be divisible by 4 but not 100
+
+                Or
+
+                Has to be divisible by 400
+
+ 
+
+                Input/Output
+                    input:
+                        1700
+                    output:
+                        1700 is not a leap year
+                
+                    input:
+                        2024
+                    output:
+                        2024 is a leap year
+             */
+        }
+    }
+}
