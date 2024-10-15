50eb8160c349f955886992051856676c56fad3b8
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-15 20:48:04 +0200

    Creating files and using system 03_ATM

---
+++ Week03/Oefeningen/03_ATM/03_ATM/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace _03_ATM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
