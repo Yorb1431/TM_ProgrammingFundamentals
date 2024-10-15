4abb75ae146597aac387c392b2d1ac8c8a85da8a
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-15 21:21:59 +0200

    finished project

--- Week03/Oefeningen/03_ATM/03_ATM/Program.cs
+++ Week03/Oefeningen/03_ATM/03_ATM/Program.cs
@@ -1,11 +1,50 @@
 ﻿using System;
+
 namespace _03_ATM
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Set the correct PIN using Convert.ToInt32
+            Console.WriteLine("Enter your correct PIN:");
+            int JuistPIN = Convert.ToInt32(Console.ReadLine());
+
+            // First attempt
+            Console.WriteLine("Enter your PIN to withdraw:");
+            int PIN = Convert.ToInt32(Console.ReadLine());
+
+            if (PIN == JuistPIN)
+            {
+                Console.WriteLine("Succesful withdrawal");
+            }
+            else
+            {
+                // Second attempt (since first attempt was wrong)
+                Console.WriteLine("Enter your PIN again:");
+                PIN = Convert.ToInt32(Console.ReadLine());
+
+                if (PIN == JuistPIN)
+                {
+                    Console.WriteLine("Succesful withdrawal");
+                }
+                else
+                {
+                    // Third attempt (since second attempt was wrong)
+                    Console.WriteLine("Enter your PIN again:");
+                    PIN = Convert.ToInt32(Console.ReadLine());
+
+                    if (PIN == JuistPIN)
+                    {
+                        Console.WriteLine("Succesful withdrawal");
+                    }
+                    else
+                    {
+                        // If all three attempts are wrong
+                        Console.WriteLine("Too many attempts");
+                    }
+                }
+            }
         }
     }
 }
