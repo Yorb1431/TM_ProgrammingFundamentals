6c0e89c70f20b4e3ff595a7e822d6a26ec10564b
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-16 09:59:48 +0200

    fix test Again fail

--- Week03/Oefeningen/03_Special/03_Special/Program.cs
+++ Week03/Oefeningen/03_Special/03_Special/Program.cs
@@ -1,26 +1,27 @@
 ﻿using System;
+
 namespace _03_Special
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Read a character from user input
             Console.Write("Enter a character: ");
-            char ch = Console.ReadKey().KeyChar; // Read a single character
-            Console.WriteLine(); // Move to the next line
+            char ch = Console.ReadKey().KeyChar; 
+            Console.WriteLine(); 
 
-            // Check if the character is a vowel
             if ("aeiouAEIOU".Contains(ch))
             {
                 Console.WriteLine("vowel");
             }
-            // Check if the character is a digit
             else if (ch >= '0' && ch <= '9')
             {
                 Console.WriteLine("digit");
             }
-            // If none of the above, it's a special character
+            else if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+            {
+                Console.WriteLine("consonant");
+            }
             else
             {
                 Console.WriteLine("special character");
