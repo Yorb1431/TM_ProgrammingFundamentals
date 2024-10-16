baad47f3086704c409fe3d9696cd40d3c5da2e3d
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-16 16:26:45 +0200

    Full assigment 03_Triangle

--- Week03/Oefeningen/03_Triangle/03_Triangle/Program.cs
+++ Week03/Oefeningen/03_Triangle/03_Triangle/Program.cs
@@ -1,11 +1,45 @@
 ﻿using System;
-namespace _03_Triangle
+
+namespace TriangleTypeChecker
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            // Read the sides of the triangle
+            double side1 = double.Parse(Console.ReadLine());
+            double side2 = double.Parse(Console.ReadLine());
+            double side3 = double.Parse(Console.ReadLine());
+
+            // Check if each side is positive
+            if (side1 <= 0)
+            {
+                Console.WriteLine("crazy input");
+            }
+            else if (side2 <= 0)
+            {
+                Console.WriteLine("crazy input");
+            }
+            else if (side3 <= 0)
+            {
+                Console.WriteLine("crazy input");
+            }
+            else
+            {
+                // Determine the type of triangle
+                if (side1 == side2 && side2 == side3)
+                {
+                    Console.WriteLine("equilateral");
+                }
+                else if (side1 == side2 || side2 == side3 || side1 == side3)
+                {
+                    Console.WriteLine("isosceles");
+                }
+                else
+                {
+                    Console.WriteLine("scalene");
+                }
+            }
         }
     }
 }
