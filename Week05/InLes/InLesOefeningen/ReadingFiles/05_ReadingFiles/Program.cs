699396c4a32787d4d4487e34f2ca591e1eda9eeb
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-17 10:35:56 +0200

    ReadingFiles finsished

--- Week05/InLes/InLesOefeningen/ReadingFiles/05_ReadingFiles/Program.cs
+++ Week05/InLes/InLesOefeningen/ReadingFiles/05_ReadingFiles/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 namespace _05_ReadingFiles
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //tekst file openen en uitlezen, ofwel eerst doc lezen dan printen of omgekeerd
+            StreamReader input = File.OpenText("Rapunzel.txt");
+            string text = input.ReadToEnd();
+            input.Close();
+            Console.WriteLine(text);
+
+
+            //nog is printen
+            input = File.OpenText("Rapunzel.txt");
+            while (!input.EndOfStream)
+            {
+                //string line = input.ReadLine();
+                //Console.WriteLine(line);
+                Console.WriteLine("-->" + input.ReadLine());
+            }
+
+
+            //character per character uitlezen en een spatie toevoegen
+            input = File.OpenText("Rapunzel.txt");
+            while (!input.EndOfStream)
+            {
+                Console.Write((char)input.Read() + " ");
+            }
+            input.Close();
+
+
+            //foreach om door tekst te lezen
+            foreach (string item in File.ReadLines("Rapunzel.txt"))
+            {
+                Console.WriteLine("-->" + item);
+            }
+
+            //character per character
+            foreach (char c in File.ReadAllText("Rapunzel.txt"))
+            {
+                Console.Write(c);
+            }
         }
     }
 }
