1d0b0d16416cd21dae2254da310fd06e8fa895b7
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-17 09:06:00 +0200

    File aanmaken and files op andere locatie

--- Week05/InLes/InLesTheoryWeek05/Program.cs
+++ Week05/InLes/InLesTheoryWeek05/Program.cs
@@ -6,7 +6,19 @@ namespace InLesTheoryWeek05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //File aanmaken
+            StreamWriter stream = File.CreateText("file.txt");
+            stream.WriteLine("Dit is tekst in in de file"); // Tekst op nieuwe lijn
+            stream.Write("Deze tekst start niet op  een niewe lijn"); //Tekst niet op nieuwe lijn
+            stream.Write("Mijn naam is P-Jonas");
+            stream.Close();
+
+
+
+            //Files op andere locaties
+            stream = File.CreateText(@"C:\TestFilePromgramming\file.txt");
+            stream.WriteLine("Tekstfile in mijn andere locatie");
+            stream.Close();
         }
     }
 }
