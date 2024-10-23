64e716951231bb11d1a9f66278621c387bc65688
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-23 09:33:06 +0200

    Finished project, finaly

--- Week04/OpdrachtOefeningen/04_Mix_it_up/04_Mix_it_up/Program.cs
+++ Week04/OpdrachtOefeningen/04_Mix_it_up/04_Mix_it_up/Program.cs
@@ -3,9 +3,37 @@ namespace _04_Mix_it_up
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            // Lees twee strings van de gebruiker
+            string woord01 = Console.ReadLine();
+            string woord02 = Console.ReadLine();
+
+            // Bepaal welk woord langer is
+            string langWoord = woord01.Length >= woord02.Length ? woord01 : woord02;
+            string kortWoord = woord01.Length < woord02.Length ? woord01 : woord02;
+
+            // Variabele om het resultaat op te slaan
+            string gemixtWoord = "";
+
+            // Loop door de karakters van beide woorden
+            for (int i = 0; i < langWoord.Length; i++)
+            {
+                // Voeg karakter van het langste woord toe als het bestaat
+                if (i < langWoord.Length)
+                {
+                    gemixtWoord += langWoord[i];
+                }
+
+                // Voeg karakter van het kortste woord toe als het bestaat
+                if (i < kortWoord.Length)
+                {
+                    gemixtWoord += kortWoord[i];
+                }
+            }
+
+            // Druk het gemixte woord af
+            Console.WriteLine(gemixtWoord);
         }
     }
 }
