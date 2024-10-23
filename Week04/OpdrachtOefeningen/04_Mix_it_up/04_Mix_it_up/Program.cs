4b297ca7753d5b9a15a8e793bc5c4e5cee59de29
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-23 09:23:24 +0200

    Creating Files 04_Mix_it_up

---
+++ Week04/OpdrachtOefeningen/04_Mix_it_up/04_Mix_it_up/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04_Mix_it_up
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
