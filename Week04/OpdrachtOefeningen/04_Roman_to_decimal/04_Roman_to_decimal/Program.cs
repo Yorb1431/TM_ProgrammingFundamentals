02842015bd26aa3dfbf35b78127387144b39f506
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-23 09:47:16 +0200

    Cyreting Files 04_Roman_to_decimal

---
+++ Week04/OpdrachtOefeningen/04_Roman_to_decimal/04_Roman_to_decimal/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04_Roman_to_decimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
