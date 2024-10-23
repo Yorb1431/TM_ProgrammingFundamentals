1b4267e30d18629128e65d09cefc2ea4da14d379
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-23 08:57:12 +0200

    Creating files 04_Largest and finishing procces

---
+++ Week04/OpdrachtOefeningen/04_Largest/04_Largest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace _04_Largest
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int Biggetal = int.MinValue;
+
+            while (true)
+            {
+                string inbput = Console.ReadLine();
+
+                if (inbput == "X")
+                {
+                    break;
+                }
+
+                int getal = Convert.ToInt32(inbput);
+
+                if (getal > Biggetal)
+                {
+                    Biggetal = getal; 
+                }
+            }
+
+            Console.WriteLine(Biggetal);
+        }
+    }
+}
