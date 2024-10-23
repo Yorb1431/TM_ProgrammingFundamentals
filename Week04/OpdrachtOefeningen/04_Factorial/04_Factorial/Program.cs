7081696e532278d7ee6b50ac5377aa497f707036
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-23 09:33:55 +0200

    creating 04_Factorial

---
+++ Week04/OpdrachtOefeningen/04_Factorial/04_Factorial/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
