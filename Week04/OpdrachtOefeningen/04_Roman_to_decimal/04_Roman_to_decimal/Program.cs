71425ca84959534bc1c155b56d353b7725af2f54
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-23 09:56:37 +0200

    Finished project + using System.Linq;

--- Week04/OpdrachtOefeningen/04_Roman_to_decimal/04_Roman_to_decimal/Program.cs
+++ Week04/OpdrachtOefeningen/04_Roman_to_decimal/04_Roman_to_decimal/Program.cs
@@ -1,11 +1,68 @@
 ﻿using System;
+using System.Linq;
 namespace _04_Roman_to_decimal
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            // Lees de Romeinse cijferinvoer van de gebruiker
+            string romanNummer = Console.ReadLine();
+
+            int decimaalGetal = 0;
+            int EerderNummer = 0;
+
+            // Loop door de Romeinse cijfers van achteren naar voren
+            for (int i = romanNummer.Length - 1; i >= 0; i--)
+            {
+                char NuChar = romanNummer.ElementAt(i);
+                int NuEnding = 0;
+
+                // Gebruik switch case om de waarde van het Romeinse cijfer te bepalen
+                switch (NuChar)
+                {
+                    case 'M':
+                        NuEnding = 1000;
+                        break;
+                    case 'D':
+                        NuEnding = 500;
+                        break;
+                    case 'C':
+                        NuEnding = 100;
+                        break;
+                    case 'L':
+                        NuEnding = 50;
+                        break;
+                    case 'X':
+                        NuEnding = 10;
+                        break;
+                    case 'V':
+                        NuEnding = 5;
+                        break;
+                    case 'I':
+                        NuEnding = 1;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Roman numeral");
+                        return; // Stop het programma als een ongeldig Romeins cijfer is ingevoerd
+                }
+
+                // Controleer of we moeten optellen of aftrekken
+                if (NuEnding < EerderNummer)
+                {
+                    decimaalGetal -= NuEnding; // Aftrekken
+                }
+                else
+                {
+                    decimaalGetal += NuEnding; // Optellen
+                }
+
+                // Update previousValue voor de volgende iteratie
+                EerderNummer = NuEnding;
+            }
+
+            // Druk de resulterende decimale waarde af
+            Console.WriteLine(decimaalGetal);
         }
     }
 }
