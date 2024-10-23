d5fb7d9724acae5b8702e9a4ece21509be1055e0
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-23 11:02:12 +0200

    Creating 04_Current_and_previous_Fibonacci

---
+++ Week04/OpdrachtOefeningen/04_Current_and_previous_Fibonacci/04_Current_and_previous_Fibonacci/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04_Current_and_previous_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
