25580c1d387b70767a209cf1fb21311ad4e4819a
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-23 08:58:49 +0200

    Creating files and using system

---
+++ Week04/OpdrachtOefeningen/04_Even_Steven/04_Even_Steven/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace _04_Even_Steven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
