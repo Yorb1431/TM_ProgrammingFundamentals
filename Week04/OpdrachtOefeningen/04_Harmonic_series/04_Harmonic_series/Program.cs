46f1e33a1955970bcc86e032e97b25d72ec6223e
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-23 11:00:25 +0200

    Finished program

--- Week04/OpdrachtOefeningen/04_Harmonic_series/04_Harmonic_series/Program.cs
+++ Week04/OpdrachtOefeningen/04_Harmonic_series/04_Harmonic_series/Program.cs
@@ -3,9 +3,33 @@ namespace _04_Harmonic_series
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int termijnen = int.Parse(Console.ReadLine());
+
+            if (termijnen <= 0)
+            {
+                Console.WriteLine("crazy input");
+                return;
+            }
+
+            double sum = 0.0;
+
+            for (int i = 1; i <= termijnen; i++)
+            {
+                sum += 1.0 / i;
+
+                if (i == termijnen)
+                {
+                    Console.Write($"1/{i}");
+                }
+                else
+                {
+                    Console.Write($"1/{i} + ");
+                }
+            }
+
+            Console.WriteLine($"\nsum = {Math.Round(sum, 5)}");
         }
     }
 }
