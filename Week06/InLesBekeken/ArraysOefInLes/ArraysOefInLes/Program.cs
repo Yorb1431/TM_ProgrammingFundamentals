6d8cb1228e7f77d8f0bf4999e4ca5853c361eb8d
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-24 11:09:02 +0200

    create files ArraysOefInLes

---
+++ Week06/InLesBekeken/ArraysOefInLes/ArraysOefInLes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ArraysOefInLes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
