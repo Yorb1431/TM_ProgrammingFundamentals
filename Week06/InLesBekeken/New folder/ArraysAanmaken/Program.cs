8a57ec86355229b6284c1b3f79e5d4e035c8b3cf
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-24 09:28:25 +0200

    Creating files ArraysAanmaken

---
+++ Week06/InLesBekeken/New folder/ArraysAanmaken/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ArraysAanmaken
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
