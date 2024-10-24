f04b569675731030aadb782736b871ced8a61540
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-24 09:05:38 +0200

    Optie 01 Foreach

--- Week06/HerhalingFilesWeek05/HerhalingWeek05Files/Program.cs
+++ Week06/HerhalingFilesWeek05/HerhalingWeek05Files/Program.cs
@@ -6,8 +6,27 @@ namespace HerhalingWeek05Files
     {
         static void Main(string[] args)
         {
+            //Optie 01
+
+            /*
             int amount = Convert.ToInt32(Console.ReadLine());
             int counter  = 0;
+
+            StreamWriter sr = File.CreateText(@"Harry Potter and the Sorcerer.txt");
+            foreach (string line in File.ReadLines($"Harry-{amount}.txt"))
+            {
+                if (counter <= amount)
+                {
+                    sr.WriteLine($"{counter} {line}");
+                    counter++;
+                }
+                else break;
+            }
+            sr.Close(); */
+
+            //Optie 02
+            
+
         }
     }
 }
