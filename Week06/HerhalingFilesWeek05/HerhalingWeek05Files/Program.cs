e60dbfb1627848d6e0c04d35ac6cf0072805fb85
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-24 09:13:46 +0200

    Optie 02  gebruik maken van while

--- Week06/HerhalingFilesWeek05/HerhalingWeek05Files/Program.cs
+++ Week06/HerhalingFilesWeek05/HerhalingWeek05Files/Program.cs
@@ -8,24 +8,33 @@ namespace HerhalingWeek05Files
         {
             //Optie 01
 
-            /*
+          
             int amount = Convert.ToInt32(Console.ReadLine());
             int counter  = 0;
-
-            StreamWriter sr = File.CreateText(@"Harry Potter and the Sorcerer.txt");
-            foreach (string line in File.ReadLines($"Harry-{amount}.txt"))
-            {
-                if (counter <= amount)
-                {
-                    sr.WriteLine($"{counter} {line}");
-                    counter++;
-                }
-                else break;
-            }
-            sr.Close(); */
+            /*
+          StreamWriter sr = File.CreateText(@"Harry Potter and the Sorcerer.txt");
+          foreach (string line in File.ReadLines($"Harry-{amount}.txt"))
+          {
+              if (counter <= amount)
+              {
+                  sr.WriteLine($"{counter} {line}");
+                  counter++;
+              }
+              else break;
+          }
+          sr.Close(); */
 
             //Optie 02
-            
+            StreamWriter schrijf = File.CreateText($"Harry-{amount}-optie02.txt");
+            StreamReader lees = File.OpenText(@"Harry Potter and the Sorcerer.txt");
+
+            counter = 0; //Resest counter
+            while (counter <= amount)
+            {
+                schrijf.WriteLine(counter + " "  + lees.ReadLine());
+            }
+            lees.Close();
+            schrijf.Close();
 
         }
     }
