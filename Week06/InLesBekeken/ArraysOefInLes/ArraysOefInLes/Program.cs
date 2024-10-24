d81163ea5b8aaf732c2aa393c896c0b9b973b2f4
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-24 11:18:47 +0200

    Create in Som

--- Week06/InLesBekeken/ArraysOefInLes/ArraysOefInLes/Program.cs
+++ Week06/InLesBekeken/ArraysOefInLes/ArraysOefInLes/Program.cs
@@ -28,6 +28,13 @@ namespace ArraysOefInLes
             //som maken en het gemiddelde berekenen
             array = new int[] { 1, 8, -3, 6, 9, 2, 3, -4, 5 };
             //neemt het origineel terug en overschrijft de array
+            int som = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                som += array[i];
+            }
+            Console.WriteLine("som: " + som);
+            Console.WriteLine("Gemiddelde: " + som / array.Length);
         }
     }
 }
