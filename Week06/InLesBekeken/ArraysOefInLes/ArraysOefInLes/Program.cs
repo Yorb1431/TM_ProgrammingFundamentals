f90df673ccd29824c5b4531c252bf2c64f1f2ef5
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-24 11:10:23 +0200

    gemakkelijke manier  arrayaanmaken

--- Week06/InLesBekeken/ArraysOefInLes/ArraysOefInLes/Program.cs
+++ Week06/InLesBekeken/ArraysOefInLes/ArraysOefInLes/Program.cs
@@ -6,6 +6,14 @@ namespace ArraysOefInLes
         static void Main(string[] args)
         {
             int[] array = { 1, 8, -3, 6, 9, 2, 3, -4, 5 };
+
+            //gemakkelijke manier
+            Console.WriteLine("som: " + array.Sum());
+            Console.WriteLine("Max: " + array.Max());
+            Console.WriteLine("Min: " + array.Min());
+            Console.WriteLine("Gemiddelde: " + array.Average());
+            Array.Sort(array);
+            Array.Reverse(array);
         }
     }
 }
