5ce2e95bab3f8699af9c9d1d86fb69a59efab4b2
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-10-24 09:39:00 +0200

    More arrays

--- Week06/InLesBekeken/New folder/ArraysAanmaken/Program.cs
+++ Week06/InLesBekeken/New folder/ArraysAanmaken/Program.cs
@@ -7,6 +7,12 @@ namespace ArraysAanmaken
         {
             //een array aanmaken
             int[] intArray = new int[10]; //alle waarden zijn 0
+            string[] stringArray = new string[4]; //alle waarden ""
+
+            double[] doubles = { 1.5, 9, 10.0, -6, 12, 5.6 };
+            Console.WriteLine(doubles.Length);
+
+            int[] ints = { 1, 3, 8, 23, 99 };
         }
     }
 }
