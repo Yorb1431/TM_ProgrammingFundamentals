5897d35611ea9b25c314b7458e1db6180362c6fb
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-08 16:36:13 +0100

    Create 05_MemyselfandI

---
+++ Week05/OefeningenWeek05/MemyselfandI/05_MemyselfandI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _05_MemyselfandI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
