82f4fb01343611e79ebfdd0b51f4d4a72f9e4db6
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-08 12:38:38 +0100

    creating files and using system;

---
+++ Week05/OefeningenWeek05/count/Week05_count/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Week05_count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
