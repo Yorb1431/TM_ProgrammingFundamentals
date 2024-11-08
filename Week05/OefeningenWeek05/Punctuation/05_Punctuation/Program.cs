4b8ad341e1b9170606aa2f67e3b1ec8151ac4d9e
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-08 15:56:38 +0100

    Creating files Punctuation

---
+++ Week05/OefeningenWeek05/Punctuation/05_Punctuation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _05_Punctuation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
