8e1631d8be149ef63387064be676e2558dffc4ed
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 12:28:00 +0100

    add files again x project

--- Week08/Recap/InLes/OefeningenHerhaling/MeMyselfAndI/MeMyselfAndI/Program.cs
+++ Week08/Recap/InLes/OefeningenHerhaling/MeMyselfAndI/MeMyselfAndI/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 namespace MeMyselfAndI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            string filenaam = Console.ReadLine();
+            Regex rx = new Regex(filenaam.ToLower());
+
+
+            string txt = File.ReadAllText(filenaam.Replace(" ", ".txt"));
+            MatchCollection matches = rx.Matches(txt);
+
+            Console.WriteLine($"{matches.Count} occourses of {filenaam}");
         }
     }
 }
