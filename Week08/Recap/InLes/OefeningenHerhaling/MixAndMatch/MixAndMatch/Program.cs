86f7efc7e7e218e46c8d039f4158220b2a16b82e
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 11:34:29 +0100

    finaly fucked worked

--- Week08/Recap/InLes/OefeningenHerhaling/MixAndMatch/MixAndMatch/Program.cs
+++ Week08/Recap/InLes/OefeningenHerhaling/MixAndMatch/MixAndMatch/Program.cs
@@ -6,7 +6,23 @@ namespace MixAndMatch
     {
         static void Main(string[] args)
         {
+            string file1 = Console.ReadLine();
+            string file2 = Console.ReadLine();
+
+            int hoeveel = Convert.ToInt32(Console.ReadLine());
+            string F1 = File.ReadAllText(file1);
+            string F2 = File.ReadAllText(file2);
+
+            for (int i = 0; i < hoeveel; i++)
+            {
+                //Console.WriteLine($"{F1[i]} + {F2[i]}"); does not work bc of ascii
+                Console.Write($"{F1[i]}");
+                Console.Write($"{F2[i]}");
+
+            }
+            Console.WriteLine();
 
         }
     }
 }
+
