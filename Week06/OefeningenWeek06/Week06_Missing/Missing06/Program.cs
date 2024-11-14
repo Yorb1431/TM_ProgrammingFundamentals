2ece3d4858bf9f52b36e9c6df642fc0aa7878cc1
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 16:10:28 +0100

    Finished 1test fail

--- Week06/OefeningenWeek06/Week06_Missing/Missing06/Program.cs
+++ Week06/OefeningenWeek06/Week06_Missing/Missing06/Program.cs
@@ -15,7 +15,48 @@ namespace Missing06
 
 
 
+            string input = Console.ReadLine();
 
+            int[] getalln = new int[input.Length];
+            int a = 0;
+            int GetalInGebruik = 0;
+
+            for (int i = 0; i <= input.Length; i++)
+            {
+                if (i < input.Length && char.IsDigit(input[i]))
+                {
+                    GetalInGebruik = GetalInGebruik * 10 + (input[i] - '0');
+                }
+                else if ((i == input.Length) || (i < input.Length && input[i] == ' '))
+                {
+                    if (GetalInGebruik != 0)
+                    {
+                        getalln[a] = GetalInGebruik;
+                        a++;
+                    }
+                    GetalInGebruik = 0;
+                }
+            }
+
+            int n = a + 1;
+            int expectedSum = n * (n + 1) / 2;
+            int som = 0;
+
+            for (int i = 0; i < a; i++)
+            {
+                som += getalln[i];
+            }
+
+            int missingNumber = expectedSum - som;
+
+            if (missingNumber == 0)
+            {
+                Console.WriteLine("There is no missing link!");
+            }
+            else
+            {
+                Console.WriteLine(missingNumber);
+            }
         }
     }
 }
