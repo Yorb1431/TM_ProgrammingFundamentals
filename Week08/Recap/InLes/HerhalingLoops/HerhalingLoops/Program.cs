cd2eec65a6f37883bae78e55df183ee2e9d45368
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 08:46:24 +0100

    Loops main

--- Week08/Recap/InLes/HerhalingLoops/HerhalingLoops/Program.cs
+++ Week08/Recap/InLes/HerhalingLoops/HerhalingLoops/Program.cs
@@ -5,7 +5,18 @@ namespace HerhalingLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             Loops
+
+            print getal 0-20
+                for( int i = 0, i <= 20, i++){
+                Console.WriteLine(i);
+            }                                   output = 1,2, 3, 4, 5, ... 18, 19, 20
+
+
+
+
+             */
         }
     }
 }
