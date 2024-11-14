deddf0f567e13c999d910afb7fa680c6564db363
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 15:53:06 +0100

    Stappenplan

--- Week06/OefeningenWeek06/Week06_Missing/Missing06/Program.cs
+++ Week06/OefeningenWeek06/Week06_Missing/Missing06/Program.cs
@@ -5,7 +5,17 @@ namespace Missing06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Stap 1: Definieer main om input te lezen
+            // Stap 2: Parse input
+            // Stap 3: gebruik 1 tot arraylengte + 1
+            // Stap 4: variabele voor som van 1 tot n
+            // Stap 5: Bereken  som van array
+            // Stap 6: Vind ontbrekend getal door werkelijke som af te trekken van verwachte som
+            // Stap 7: print output
+
+
+
+
         }
     }
 }
