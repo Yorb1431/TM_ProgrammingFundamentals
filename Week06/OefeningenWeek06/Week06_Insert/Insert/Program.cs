b1c1e2a5e0ff1c1cca34984b547743506d07c1df
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 16:35:29 +0100

    Finished product

--- Week06/OefeningenWeek06/Week06_Insert/Insert/Program.cs
+++ Week06/OefeningenWeek06/Week06_Insert/Insert/Program.cs
@@ -5,7 +5,51 @@ namespace Insert
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Stap 1: Lees  array en waarde
+            // Stap 2: Zoek  positie voor de waarde in array
+            // Stap 3: Verschuif elementen na positie één plek op
+            // Stap 4: Plaats op de juiste positie
+            // Stap 5: Print array
+
+
+            string input = Console.ReadLine();
+            int waarde = int.Parse(Console.ReadLine());
+
+            string[] iArray = input.Split(' ');
+            int[] gesorteerd = new int[iArray.Length];
+
+            for (int i = 0; i < iArray.Length; i++)
+            {
+                gesorteerd[i] = int.Parse(iArray[i]);
+            }
+
+            int index = 0;
+            while (index < gesorteerd.Length && gesorteerd[index] < waarde)
+            {
+                index++;
+            }
+
+            int[] nieuwewArray = new int[gesorteerd.Length + 1];
+            for (int i = 0; i < index; i++)
+            {
+                nieuwewArray[i] = gesorteerd[i];
+            }
+
+            nieuwewArray[index] = waarde;
+
+            for (int i = index; i < gesorteerd.Length; i++)
+            {
+                nieuwewArray[i + 1] = gesorteerd[i];
+            }
+
+            for (int i = 0; i < nieuwewArray.Length; i++)
+            {
+                Console.Write(nieuwewArray[i]);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+
         }
     }
 }
