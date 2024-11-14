208d054302e54d9a904ef3cf03beec6b66b423d6
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 10:51:18 +0100

    create FloydsTriangle

---
+++ Week08/Recap/InLes/OefeningenHerhaling/FloydsTriangle/FloydsTriangle/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FloydsTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
