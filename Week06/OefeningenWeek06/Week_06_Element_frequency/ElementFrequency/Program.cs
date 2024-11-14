3e83ca74dc01b910dfbd8b9b665778b466eff39a
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 17:28:55 +0100

    Comments

--- Week06/OefeningenWeek06/Week_06_Element_frequency/ElementFrequency/Program.cs
+++ Week06/OefeningenWeek06/Week_06_Element_frequency/ElementFrequency/Program.cs
@@ -5,7 +5,11 @@ namespace ElementFrequency
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Stap 1: Lees array  karakters
+            // Stap 2: Maak frequentiearray van ASCII-waarde
+            // Stap 3: Tel frequentie van elk karakter
+            // Stap 4: Print karakters met hun frequentie
+
         }
     }
 }
