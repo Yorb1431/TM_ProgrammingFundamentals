fee4371ec3f5c7c0ff20d2b3ef043a7f6a2173e8
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 10:26:42 +0100

    Using system;

--- Week06/OefeningenWeek06/Week06_Ending/Week06_Ending/Program.cs
+++ Week06/OefeningenWeek06/Week06_Ending/Week06_Ending/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Week06_Ending
+﻿using System;
+namespace Week06_Ending
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             Read in two strings (separated by a space) and find the longest common ending between two given strings.
+
+            Note: use of Linq-library or Array-library is forbidden!
+
+
+            Input/Output
+            input:
+            dream stream
+            output:
+            ream
+
+            input:
+            cornflakes kellogs
+            output:
+            s
+             */
+
+            
+
+
         }
     }
 }
