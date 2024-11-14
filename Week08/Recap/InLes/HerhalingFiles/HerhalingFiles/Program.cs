9848016b9cd3b47ca14999fbc686e0dd106cd66a
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 09:16:27 +0100

    using System.IO;

--- Week08/Recap/InLes/HerhalingFiles/HerhalingFiles/Program.cs
+++ Week08/Recap/InLes/HerhalingFiles/HerhalingFiles/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.IO;
 namespace HerhalingFiles
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
 }
