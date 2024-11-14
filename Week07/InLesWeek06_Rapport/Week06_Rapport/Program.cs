3c4ce53bdd6324a51443bc037d1a29b913521a50
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 10:14:14 +0100

    ﻿using System; using System.Runtime.Serialization;

---
+++ Week07/InLesWeek06_Rapport/Week06_Rapport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.Serialization;
+namespace Week06_Rapport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
