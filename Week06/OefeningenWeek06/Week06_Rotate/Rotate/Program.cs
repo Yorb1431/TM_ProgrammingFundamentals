f9bd2c8321be4f75a7915b06adfb4ed353b46709
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 17:25:58 +0100

    Finished producht

--- Week06/OefeningenWeek06/Week06_Rotate/Rotate/Program.cs
+++ Week06/OefeningenWeek06/Week06_Rotate/Rotate/Program.cs
@@ -11,6 +11,31 @@ namespace Rotate
             // Stap 4: Maak nieuwe array met geroteerde elementen
             // Stap 5: Print nieuwe array
 
+
+            string input = Console.ReadLine();
+            string[] iArray = input.Split(' ');
+            int[] array = new int[iArray.Length];
+
+            for (int i = 0; i < iArray.Length; i++)
+            {
+                array[i] = int.Parse(iArray[i]);
+            }
+
+            int rotatie = int.Parse(Console.ReadLine());
+
+            int[] gedraaaideArrey = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int index = (i - rotatie + array.Length) % array.Length;
+                gedraaaideArrey[index] = array[i];
+            }
+
+            for (int i = 0; i < gedraaaideArrey.Length; i++)
+            {
+                Console.Write(gedraaaideArrey[i] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
