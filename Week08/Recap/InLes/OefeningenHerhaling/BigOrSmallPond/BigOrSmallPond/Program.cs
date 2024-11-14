abff1428d6d321345a3a4d67d5f4fcf2975f40e8
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 10:36:43 +0100

    Create BigOrSmallPond

---
+++ Week08/Recap/InLes/OefeningenHerhaling/BigOrSmallPond/BigOrSmallPond/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BigOrSmallPond
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
