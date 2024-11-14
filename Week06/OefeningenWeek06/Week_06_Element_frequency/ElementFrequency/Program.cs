7e48d87c754685b2ff5a1a20880241e20a1824c0
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 17:32:55 +0100

    Finsihed product

--- Week06/OefeningenWeek06/Week_06_Element_frequency/ElementFrequency/Program.cs
+++ Week06/OefeningenWeek06/Week_06_Element_frequency/ElementFrequency/Program.cs
@@ -10,6 +10,22 @@ namespace ElementFrequency
             // Stap 3: Tel frequentie van elk karakter
             // Stap 4: Print karakters met hun frequentie
 
+            string input = Console.ReadLine();
+            char[] ch = input.ToCharArray();
+
+            int[] frequentie = new int[256];
+            for (int i = 0; i < ch.Length; i++)
+            {
+                frequentie[ch[i]]++;
+            }
+
+            for (int i = 0; i < frequentie.Length; i++)
+            {
+                if (frequentie[i] > 0)
+                {
+                    Console.WriteLine($"Character '{(char)i}': {frequentie[i]} times");
+                }
+            }
         }
     }
 }
