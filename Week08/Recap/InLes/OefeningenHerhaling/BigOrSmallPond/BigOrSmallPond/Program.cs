fed1cbb149465f285e8cd42950d0a50f9293cc94
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 10:50:35 +0100

    Final  Project

--- Week08/Recap/InLes/OefeningenHerhaling/BigOrSmallPond/BigOrSmallPond/Program.cs
+++ Week08/Recap/InLes/OefeningenHerhaling/BigOrSmallPond/BigOrSmallPond/Program.cs
@@ -5,7 +5,28 @@ namespace BigOrSmallPond
     {
         static void Main(string[] args)
         {
+            int diepte = Convert.ToInt32 (Console.ReadLine());
 
+            for (int i = 1; i <= diepte; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write($"0 ");
+                }
+                    for (int k = diepte; k > i; k--)
+                {
+                    Console.Write($"- ");
+                }
+                    for (int l = diepte; l > i; l--)
+                {
+                    Console.Write($"- ");
+                }
+                    for (int m = 0; m < i; m++)
+                {
+                    Console.Write($"0 ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
