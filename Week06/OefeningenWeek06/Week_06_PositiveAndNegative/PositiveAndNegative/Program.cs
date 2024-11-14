aed385ca05f97419673a0475027b133b88d77c7d
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 17:34:52 +0100

    create PositiveAndNegative

---
+++ Week06/OefeningenWeek06/Week_06_PositiveAndNegative/PositiveAndNegative/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PositiveAndNegative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
