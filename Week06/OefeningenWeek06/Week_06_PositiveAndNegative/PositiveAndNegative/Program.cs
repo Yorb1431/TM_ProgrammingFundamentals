bbb4de3c6e96d108f40f2132e9a39fea70e93e40
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 17:44:31 +0100

    first test

--- Week06/OefeningenWeek06/Week_06_PositiveAndNegative/PositiveAndNegative/Program.cs
+++ Week06/OefeningenWeek06/Week_06_PositiveAndNegative/PositiveAndNegative/Program.cs
@@ -10,6 +10,48 @@ namespace PositiveAndNegative
             // Stap 3: Loop door array | voeg getallen toe aan de juiste array
             // Stap 4: Print arrays
 
+
+            string input = Console.ReadLine();
+            char[] grens = new char[] { ' ' };
+            string[] stringVanNumbers = input.Split(grens);
+
+            int[] positiveGetalllen = new int[stringVanNumbers.Length];
+            int[] negativeGetallen = new int[stringVanNumbers.Length];
+            int positieveTelling = 0;
+            int negatieVeteller = 0;
+
+            for (int i = 0; i < stringVanNumbers.Length; i++)
+            {
+                int getal = int.Parse(stringVanNumbers[i]);
+
+                if (getal >= 0)
+                {
+                    positiveGetalllen[positieveTelling] = getal;
+                    positieveTelling++;
+                }
+                else
+                {
+                    negativeGetallen[negatieVeteller] = getal;
+                    negatieVeteller++;
+                }
+            }
+
+            for (int i = 0; i < positieveTelling; i++)
+            {
+                Console.Write(positiveGetalllen[i]);
+                if (i < positieveTelling - 1)
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < negatieVeteller; i++)
+            {
+                Console.Write(negativeGetallen[i]);
+                if (i < negatieVeteller - 1)
+                    Console.Write(" ");
+            }
+            Console.Write(" ");
+            Console.WriteLine();
         }
     }
 }
