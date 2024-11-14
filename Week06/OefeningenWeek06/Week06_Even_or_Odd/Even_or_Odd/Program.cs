ca9c7503952bdfec992fbe780c40954c32b93bfd
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 16:39:09 +0100

    Comments

--- Week06/OefeningenWeek06/Week06_Even_or_Odd/Even_or_Odd/Program.cs
+++ Week06/OefeningenWeek06/Week06_Even_or_Odd/Even_or_Odd/Program.cs
@@ -5,7 +5,13 @@ namespace Even_or_Odd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Stap 1: Lees array
+            // Stap 2: Loop door getal en controleer of het even of oneven is
+            // Stap 3: Vervang getallen door "Even" of "Odd"
+            // Stap 4: Print array
+
+
+
         }
     }
 }
