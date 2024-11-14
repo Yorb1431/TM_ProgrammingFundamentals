a1d6eddf322f88294f39de3795f78b7b75704ac2
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 11:58:12 +0100

    add txt files

--- Week08/Recap/InLes/OefeningenHerhaling/Reverse/Reverse/Program.cs
+++ Week08/Recap/InLes/OefeningenHerhaling/Reverse/Reverse/Program.cs
@@ -6,7 +6,12 @@ namespace Reverse
     {
         static void Main(string[] args)
         {
+            string filenaam = Console.ReadLine();
+            string revered = "";
 
+            foreach (var line in File.ReadLines(filenaam)) { 
+                
+            }
         }
     }
 }
