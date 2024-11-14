3d0b889fcf5a798de575b8b9df08664380316367
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 09:12:50 +0100

    Create HerhalingFiles

---
+++ Week08/Recap/InLes/HerhalingFiles/HerhalingFiles/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HerhalingFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
