880ec366058cffdb9b86c68e4e7828cebe3c4c55
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 11:00:25 +0100

    final project

--- Week08/Recap/InLes/OefeningenHerhaling/FloydsTriangle/FloydsTriangle/Program.cs
+++ Week08/Recap/InLes/OefeningenHerhaling/FloydsTriangle/FloydsTriangle/Program.cs
@@ -5,7 +5,19 @@ namespace FloydsTriangle
     {
         static void Main(string[] args)
         {
-            
+            int lijnen = Convert.ToInt32(Console.ReadLine());
+            int teller = 1;
+
+            for (int i = 1; i <= lijnen; i++)
+            {
+                // Adjust inner loop to run 'i' times for each row
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write($"{teller} ");
+                    teller++;
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
