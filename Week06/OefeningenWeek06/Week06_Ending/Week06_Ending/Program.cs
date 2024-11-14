4cd6b0919104696745a1403cdc15dbe2b993c833
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 10:35:25 +0100

    final project

--- Week06/OefeningenWeek06/Week06_Ending/Week06_Ending/Program.cs
+++ Week06/OefeningenWeek06/Week06_Ending/Week06_Ending/Program.cs
@@ -5,27 +5,42 @@ namespace Week06_Ending
     {
         static void Main(string[] args)
         {
-            /*
-             Read in two strings (separated by a space) and find the longest common ending between two given strings.
+            // Read input
+            string invoer = Console.ReadLine();
 
-            Note: use of Linq-library or Array-library is forbidden!
+            // Split input into two words
+            string[] woorden = invoer.Split(' ');
+            string nummer1 = woorden[0];
+            string Wooord2 = woorden[1];
 
+            int lengte1 = nummer1.Length;
+            int lengte2 = Wooord2.Length;
+            int matchLengte = 0;
 
-            Input/Output
-            input:
-            dream stream
-            output:
-            ream
+            // Loop from the end of both strings to find common ending
+            while (matchLengte < lengte1 && matchLengte < lengte2)
+            {
+                if (nummer1[lengte1 - 1 - matchLengte] == Wooord2[lengte2 - 1 - matchLengte])
+                {
+                    matchLengte++;
+                }
+                else
+                {
+                    break;
+                }
+            }
 
-            input:
-            cornflakes kellogs
-            output:
-            s
-             */
+            // Output the common ending if found
+            if (matchLengte > 0)
+            {
+                string gemeenschappelijkEinde = nummer1.Substring(lengte1 - matchLengte);
+                Console.WriteLine(gemeenschappelijkEinde);
+            }
+            else
+            {
+                Console.WriteLine("output:");
 
-            
-
-
+            }
         }
     }
 }
