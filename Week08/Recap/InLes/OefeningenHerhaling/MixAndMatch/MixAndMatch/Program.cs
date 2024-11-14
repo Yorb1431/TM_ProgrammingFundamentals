81307085a46d5b3f0e283983db62d6885aed677c
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 11:00:59 +0100

    create MixAndMatch

---
+++ Week08/Recap/InLes/OefeningenHerhaling/MixAndMatch/MixAndMatch/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MixAndMatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
