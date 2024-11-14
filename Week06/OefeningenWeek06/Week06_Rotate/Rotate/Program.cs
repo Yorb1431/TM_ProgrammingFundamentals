da0794febbeb68da470fa35998d8cebcc6b51f03
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 17:19:56 +0100

    comments

--- Week06/OefeningenWeek06/Week06_Rotate/Rotate/Program.cs
+++ Week06/OefeningenWeek06/Week06_Rotate/Rotate/Program.cs
@@ -5,7 +5,12 @@ namespace Rotate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Stap 1: Lees array
+            // Stap 2: Lees aantal rotaties
+            // Stap 3: Bereken nieuwe index voor elk element
+            // Stap 4: Maak nieuwe array met geroteerde elementen
+            // Stap 5: Print nieuwe array
+
         }
     }
 }
