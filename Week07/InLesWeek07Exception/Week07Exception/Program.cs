dfd26f7f023ee874d511d7b3c94aff643190bba6
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 10:16:38 +0100

    Full programm

--- Week07/InLesWeek07Exception/Week07Exception/Program.cs
+++ Week07/InLesWeek07Exception/Week07Exception/Program.cs
@@ -5,7 +5,56 @@ namespace Week07Exception
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*Console.WriteLine("Geef een aantal lijnen in: ");
+            int nr = Convert.ToInt32(Console.ReadLine());
+
+            StreamReader file = File.OpenText("english.txt");
+            for (int i = 0; i < nr; i++)
+            {
+                Console.WriteLine(file.ReadLine());
+            }*/
+
+            Console.WriteLine("Geef een aantal lijnen: ");
+            string antwoord = Console.ReadLine();
+
+            if (Int32.TryParse(antwoord, out int nr))
+            {
+                StreamReader file = File.OpenText("english.txt");
+                for (int i = 0; i < nr; i++)
+                {
+                    Console.WriteLine(file.ReadLine());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Verkeerd getal");
+            }
+
+
+            //EXCEPTION HANDELING
+            Console.WriteLine();
+            Console.WriteLine("Geef een aantal lijnen: ");
+            antwoord = Console.ReadLine();
+            if (Int32.TryParse(antwoord, out nr))
+            {
+                try
+                {
+                    StreamReader f = File.OpenText("englih.txt");
+                    for (int i = 0; i < nr; i++)
+                    {
+                        Console.WriteLine(f.ReadLine());
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oeps: " + e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Verkeerd getal");
+            }
+
         }
     }
 }
