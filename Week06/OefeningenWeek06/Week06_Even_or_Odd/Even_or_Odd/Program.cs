d8fba143f068e210e2a7d3502f6a39dfac01024f
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 16:53:02 +0100

    try fix sapcing bug

--- Week06/OefeningenWeek06/Week06_Even_or_Odd/Even_or_Odd/Program.cs
+++ Week06/OefeningenWeek06/Week06_Even_or_Odd/Even_or_Odd/Program.cs
@@ -11,7 +11,58 @@ namespace Even_or_Odd
             // Stap 4: Print array
 
 
+            string input = Console.ReadLine();
+            int[] getal = new int[100];
+            int som = 0;
+            string nieuwgetal = "";
 
+            for (int i = 0; i < input.Length; i++)
+            {
+                char ch = input[i];
+
+                if (ch == ' ')
+                {
+                    if (!string.IsNullOrEmpty(nieuwgetal))
+                    {
+                        getal[som] = int.Parse(nieuwgetal);
+                        som++;
+                        nieuwgetal = "";
+                    }
+                }
+                else
+                {
+                    nieuwgetal += ch;
+                }
+            }
+
+            if (!string.IsNullOrEmpty(nieuwgetal))
+            {
+                getal[som] = int.Parse(nieuwgetal);
+                som++;
+            }
+
+            string[] output = new string[som];
+            for (int i = 0; i < som; i++)
+            {
+                if (getal[i] % 2 == 0)
+                {
+                    output[i] = "Even";
+                }
+                else
+                {
+                    output[i] = "Odd";
+                }
+            }
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                Console.Write(output[i]);
+                if (i < output.Length - 1)
+                {
+                    Console.Write(" "); 
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
