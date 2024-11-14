496c48131de8a7386c93f8a2d28e716f54b191eb
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-14 13:23:23 +0100

    test comit

---
+++ Week08/Recap/InLes/OefeningenHerhaling/test/test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filenaam = Console.ReadLine();
+            Regex rx = new Regex(filenaam.ToLower());
+
+            string txt = File.ReadAllText(filenaam.Replace(" ", "") + ".txt");
+            MatchCollection matches = rx.Matches(txt);
+
+            Console.WriteLine($"{matches.Count} occourses of {filenaam}");
+        }
+    }
+}
