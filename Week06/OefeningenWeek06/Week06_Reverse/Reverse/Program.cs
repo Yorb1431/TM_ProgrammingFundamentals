8a5e6b55c1bb988af086871bf1f7a5f3bb96d042
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-15 13:47:09 +0100

    final  project  week 06

--- Week06/OefeningenWeek06/Week06_Reverse/Reverse/Program.cs
+++ Week06/OefeningenWeek06/Week06_Reverse/Reverse/Program.cs
@@ -12,6 +12,41 @@ namespace Reverse
             // Stap 4: Sla omgekeerde woorden in de nieuwe array
             // Stap 5: Print nieuwe array
 
+
+            string input = Console.ReadLine();
+            string omgedraaid = "";
+            string woord = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char ch = input[i];
+
+                if (ch == ' ' || i == input.Length - 1)
+                {
+                    if (i == input.Length - 1 && ch != ' ')
+                    {
+                        woord += ch;
+                    }
+
+                    for (int j = woord.Length - 1; j >= 0; j--)
+                    {
+                        omgedraaid += woord[j];
+                    }
+
+                    if (ch == ' ')
+                    {
+                        omgedraaid += " ";
+                    }
+
+                    woord = "";
+                }
+                else
+                {
+                    woord += ch;
+                }
+            }
+
+            Console.WriteLine($"{omgedraaid} ");
         }
     }
 }
