f0a8e80c35379b166d1360510984cb434d6ba5a4
Yorbe van der Mast <yorbevandermast@gmail.com>
2024-11-15 13:36:30 +0100

    Comments

--- Week06/OefeningenWeek06/Week06_Reverse/Reverse/Program.cs
+++ Week06/OefeningenWeek06/Week06_Reverse/Reverse/Program.cs
@@ -5,7 +5,13 @@ namespace Reverse
     {
         static void Main(string[] args)
         {
-            
+            // Stap 1: Lees string
+            // Stap 2: splits in array van woorden
+            // Stap 3: Maak nieuwe array voor omgekeerde woorden
+            // Stap 3: Loop  woorden en keer elk woord om
+            // Stap 4: Sla omgekeerde woorden in de nieuwe array
+            // Stap 5: Print nieuwe array
+
         }
     }
 }
